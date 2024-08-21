955628e3b41ddff3f3c1eaafd04425a71c8dc150
“MonkeYor13” <monkeyor13@gmail.com>
2024-08-21 18:52:28 -0300

    create metacritic App

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, ScrollView, StatusBar } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
+
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor='green' />
+      <ScrollView>
+        {games.map((game) => (
+          <View key={game.slug} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image} />
+            <Text style={styles.title}>{game.title}</Text>
+            <Text style={styles.description}>{game.description}</Text>
+            <Text style={styles.score}>{game.score}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +32,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+  },
+  card: {
+    marginBottom: 42,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+  },
+  score: {
+    fontSize: 20,
+    color: "green",
+    fontWeight: "bold",
+    marginTop: 10,
   },
 });
